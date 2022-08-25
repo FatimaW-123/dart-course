6341da1cb19d6757b5325562911dfa109638717c
myaccount-94 <tryt960@gmail.com>
2022-08-25 06:29:10 +0300

    solved lec1_task2

--- task/1/2.dart
+++ task/1/2.dart
@@ -10,23 +10,25 @@ void main(List<String> args) {
   print(x==y);
 
   print("is x bigger than y:");
-  print(  );
+  print(x>y);
 
   print("is x not equal y:");
-  print(  );
+  print(x!=y);
 
   print("is x less than or equal y:");
-  print(  );
+  print(x<=y);
 
   print("the value of 5 plus 2 is");
   print(5+2);
 
   print("the value of 3 multiply 6 is");
-  print( );
+  print(3*6);
 
   print("the value of 5 divide by 2 is");
-  print( );
+  print(5/2);
 
   print("the integer value of 5 divide by 2 is");
-  print( );
+  print(5~/2);
+
+
 }
